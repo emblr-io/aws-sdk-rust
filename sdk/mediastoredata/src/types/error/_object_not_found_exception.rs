5229aacb82bab82516b9ab26e45998318eb2b5ff
Conner Holden <conner@emblr.io>
2025-07-07 18:33:34 -0600

    Add serde support for mediastoredata

--- sdk/mediastoredata/src/types/error/_object_not_found_exception.rs
+++ sdk/mediastoredata/src/types/error/_object_not_found_exception.rs
@@ -2,6 +2,8 @@
 
 /// <p>Could not perform an operation on an object that does not exist.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct ObjectNotFoundException {
     #[allow(missing_docs)] // documentation missing in model
