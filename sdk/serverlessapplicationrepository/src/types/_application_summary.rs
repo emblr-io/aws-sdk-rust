9c06417b011b850dc37743b6740a7f7d315930f2
Conner Holden <conner@emblr.io>
2025-07-07 18:34:11 -0600

    Add serde support for serverlessapplicationrepository

--- sdk/serverlessapplicationrepository/src/types/_application_summary.rs
+++ sdk/serverlessapplicationrepository/src/types/_application_summary.rs
@@ -2,6 +2,8 @@
 
 /// <p>Summary of details about the application.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct ApplicationSummary {
     /// <p>The application Amazon Resource Name (ARN).</p>
--- sdk/serverlessapplicationrepository/src/types/error/_not_found_exception.rs
+++ sdk/serverlessapplicationrepository/src/types/error/_not_found_exception.rs
@@ -2,6 +2,8 @@
 
 /// <p>The resource (for example, an access policy statement) specified in the request doesn't exist.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct NotFoundException {
     /// <p>404</p>
