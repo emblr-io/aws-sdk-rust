8aa2facd41f6fe540283722e66a3d4d9a45a408a
Conner Holden <conner@emblr.io>
2025-07-07 18:31:55 -0600

    Add serde support for artifact

--- sdk/artifact/src/types/_report_summary.rs
+++ sdk/artifact/src/types/_report_summary.rs
@@ -2,6 +2,8 @@
 
 /// <p>Summary for report resource.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct ReportSummary {
     /// <p>Unique resource ID for the report resource.</p>
