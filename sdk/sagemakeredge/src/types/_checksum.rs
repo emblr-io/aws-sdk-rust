1caf207175492fa45b8a0e40626cb9fbf80aab33
Conner Holden <conner@emblr.io>
2025-07-07 18:34:07 -0600

    Add serde support for sagemakeredge

--- sdk/sagemakeredge/src/types/_checksum.rs
+++ sdk/sagemakeredge/src/types/_checksum.rs
@@ -2,6 +2,8 @@
 
 /// <p>Information about the checksum of a model deployed on a device.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct Checksum {
     /// <p>The type of the checksum.</p>
--- sdk/sagemakeredge/src/types/_edge_metric.rs
+++ sdk/sagemakeredge/src/types/_edge_metric.rs
@@ -2,6 +2,8 @@
 
 /// <p>Information required for edge device metrics.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct EdgeMetric {
     /// <p>The dimension of metrics published.</p>
