e256f3f44e528f505702d5430d0ab3f07dc229bd
Conner Holden <conner@emblr.io>
2025-07-07 18:34:24 -0600

    Add serde support for timestreaminfluxdb

--- sdk/timestreaminfluxdb/src/types/_db_parameter_group_summary.rs
+++ sdk/timestreaminfluxdb/src/types/_db_parameter_group_summary.rs
@@ -2,6 +2,8 @@
 
 /// <p>Contains a summary of a DB parameter group.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct DbParameterGroupSummary {
     /// <p>A service-generated unique identifier.</p>
