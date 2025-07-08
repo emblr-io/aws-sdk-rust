540f2676dfde1bb4509a1b7ff5c125142cf3b36b
Conner Holden <conner@emblr.io>
2025-07-07 18:32:19 -0600

    Add serde support for cloud9

--- sdk/cloud9/src/types/_environment_member.rs
+++ sdk/cloud9/src/types/_environment_member.rs
@@ -2,6 +2,8 @@
 
 /// <p>Information about an environment member for an Cloud9 development environment.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct EnvironmentMember {
     /// <p>The type of environment member permissions associated with this environment member. Available values include:</p>
--- sdk/cloud9/src/types/error/_too_many_requests_exception.rs
+++ sdk/cloud9/src/types/error/_too_many_requests_exception.rs
@@ -2,6 +2,8 @@
 
 /// <p>Too many service requests were made over the given time period.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct TooManyRequestsException {
     #[allow(missing_docs)] // documentation missing in model
