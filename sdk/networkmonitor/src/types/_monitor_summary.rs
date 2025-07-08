7e11ea19560f6adba130c1e3bb2207fc30be05c2
Conner Holden <conner@emblr.io>
2025-07-07 18:33:42 -0600

    Add serde support for networkmonitor

--- sdk/networkmonitor/src/types/_monitor_summary.rs
+++ sdk/networkmonitor/src/types/_monitor_summary.rs
@@ -2,6 +2,8 @@
 
 /// <p>Displays summary information about a monitor.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct MonitorSummary {
     /// <p>The ARN of the monitor.</p>
--- sdk/networkmonitor/src/types/_probe.rs
+++ sdk/networkmonitor/src/types/_probe.rs
@@ -2,6 +2,8 @@
 
 /// <p>Describes information about a network monitor probe.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct Probe {
     /// <p>The ID of the probe.</p>
