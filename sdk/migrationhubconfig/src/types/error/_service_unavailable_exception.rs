31b46942824371ded225009273b2a0e892889fd5
Conner Holden <conner@emblr.io>
2025-07-07 18:33:36 -0600

    Add serde support for migrationhubconfig

--- sdk/migrationhubconfig/src/types/error/_service_unavailable_exception.rs
+++ sdk/migrationhubconfig/src/types/error/_service_unavailable_exception.rs
@@ -2,6 +2,8 @@
 
 /// <p>Exception raised when a request fails due to temporary unavailability of the service.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct ServiceUnavailableException {
     #[allow(missing_docs)] // documentation missing in model
