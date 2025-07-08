17b24c58d350466821b256661b51af630b27d3d1
Conner Holden <conner@emblr.io>
2025-07-07 18:32:35 -0600

    Add serde support for connectcontactlens

--- sdk/connectcontactlens/src/types/error/_internal_service_exception.rs
+++ sdk/connectcontactlens/src/types/error/_internal_service_exception.rs
@@ -2,6 +2,8 @@
 
 /// <p>Request processing failed due to an error or failure with the service.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct InternalServiceException {
     #[allow(missing_docs)] // documentation missing in model
