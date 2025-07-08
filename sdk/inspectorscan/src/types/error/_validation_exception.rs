a75cb2bc5668ec17b26d5adc742fc16264c6a06d
Conner Holden <conner@emblr.io>
2025-07-07 18:33:08 -0600

    Add serde support for inspectorscan

--- sdk/inspectorscan/src/types/error/_validation_exception.rs
+++ sdk/inspectorscan/src/types/error/_validation_exception.rs
@@ -2,6 +2,8 @@
 
 /// <p>The request has failed validation due to missing required fields or having invalid inputs.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct ValidationException {
     #[allow(missing_docs)] // documentation missing in model
