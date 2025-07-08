6a7317e4862395b5eec7efee4f89ff296ae482cb
Conner Holden <conner@emblr.io>
2025-07-07 18:34:07 -0600

    Add serde support for sagemakera2iruntime

--- sdk/sagemakera2iruntime/src/types/_human_loop_summary.rs
+++ sdk/sagemakera2iruntime/src/types/_human_loop_summary.rs
@@ -2,6 +2,8 @@
 
 /// <p>Summary information about the human loop.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct HumanLoopSummary {
     /// <p>The name of the human loop.</p>
