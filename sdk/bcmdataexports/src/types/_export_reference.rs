9c014c80380cf884ecedea7e0f4416ab1c065554
Conner Holden <conner@emblr.io>
2025-07-07 18:32:12 -0600

    Add serde support for bcmdataexports

--- sdk/bcmdataexports/src/types/_export_reference.rs
+++ sdk/bcmdataexports/src/types/_export_reference.rs
@@ -2,6 +2,8 @@
 
 /// <p>The reference details for a given export.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct ExportReference {
     /// <p>The Amazon Resource Name (ARN) for this export.</p>
--- sdk/bcmdataexports/src/types/_export_status.rs
+++ sdk/bcmdataexports/src/types/_export_status.rs
@@ -2,6 +2,8 @@
 
 /// <p>The status of the data export.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct ExportStatus {
     /// <p>The status code for the request.</p>
