0bdc3b7b4026aaf1f935a050a74fca569dd9d536
Conner Holden <conner@emblr.io>
2025-07-07 18:33:11 -0600

    Add serde support for iotjobsdataplane

--- sdk/iotjobsdataplane/src/types/error/_terminal_state_exception.rs
+++ sdk/iotjobsdataplane/src/types/error/_terminal_state_exception.rs
@@ -2,6 +2,8 @@
 
 /// <p>The job is in a terminal state.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct TerminalStateException {
     #[allow(missing_docs)] // documentation missing in model
