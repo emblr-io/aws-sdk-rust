0c6948b1bdbb3901bdef14d70a1ae2b49b887327
Conner Holden <conner@emblr.io>
2025-07-07 18:34:00 -0600

    Add serde support for repostspace

--- sdk/repostspace/src/types/_validation_exception_field.rs
+++ sdk/repostspace/src/types/_validation_exception_field.rs
@@ -2,6 +2,8 @@
 
 /// <p>Stores information about a field that’s passed inside a request that resulted in an exception.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct ValidationExceptionField {
     /// <p>Message describing why the field failed validation.</p>
