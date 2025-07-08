e6b38d8b90fcf4e8b655e87fa056c7b5487b3d3d
Conner Holden <conner@emblr.io>
2025-07-07 18:31:50 -0600

    Add serde support for applicationcostprofiler

--- sdk/applicationcostprofiler/src/types/_report_definition.rs
+++ sdk/applicationcostprofiler/src/types/_report_definition.rs
@@ -2,6 +2,8 @@
 
 /// <p>The configuration of a report in AWS Application Cost Profiler.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct ReportDefinition {
     /// <p>The ID of the report.</p>
--- sdk/applicationcostprofiler/src/types/_source_s3_location.rs
+++ sdk/applicationcostprofiler/src/types/_source_s3_location.rs
@@ -2,6 +2,8 @@
 
 /// <p>Represents the Amazon Simple Storage Service (Amazon S3) location where usage data is read from.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct SourceS3Location {
     /// <p>Name of the bucket.</p>
