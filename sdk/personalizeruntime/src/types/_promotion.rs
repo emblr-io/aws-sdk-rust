b36bd2594e3dabeea52cecb4885e2949c388501b
Conner Holden <conner@emblr.io>
2025-07-07 18:33:49 -0600

    Add serde support for personalizeruntime

--- sdk/personalizeruntime/src/types/_promotion.rs
+++ sdk/personalizeruntime/src/types/_promotion.rs
@@ -2,6 +2,8 @@
 
 /// <p>Contains information on a promotion. A promotion defines additional business rules that apply to a configurable subset of recommended items.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct Promotion {
     /// <p>The name of the promotion.</p>
