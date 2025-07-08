2b6af840d7688c59d2c3a1e7b62856dfe173e409
Conner Holden <conner@emblr.io>
2025-07-07 18:34:31 -0600

    Add serde support for workspacesthinclient

--- sdk/workspacesthinclient/src/types/_software_set_summary.rs
+++ sdk/workspacesthinclient/src/types/_software_set_summary.rs
@@ -2,6 +2,8 @@
 
 /// <p>Describes a software set.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct SoftwareSetSummary {
     /// <p>The ID of the software set.</p>
