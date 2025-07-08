d0e983f5b7bd14d56ab412660ced22815429dc1f
Conner Holden <conner@emblr.io>
2025-07-07 18:34:09 -0600

    Add serde support for schemas

--- sdk/schemas/src/operation/update_registry/_update_registry_input.rs
+++ sdk/schemas/src/operation/update_registry/_update_registry_input.rs
@@ -2,6 +2,8 @@
 
 /// <p>Updates the registry.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct UpdateRegistryInput {
     /// <p>The description of the registry to update.</p>
--- sdk/schemas/src/types/_schema_summary.rs
+++ sdk/schemas/src/types/_schema_summary.rs
@@ -2,6 +2,8 @@
 
 /// <p>A summary of schema details.</p>
 #[non_exhaustive]
+#[cfg_attr(feature = "serde-serialize", derive(::serde::Serialize))]
+#[cfg_attr(feature = "serde-deserialize", derive(::serde::Deserialize))]
 #[derive(::std::clone::Clone, ::std::cmp::PartialEq, ::std::fmt::Debug)]
 pub struct SchemaSummary {
     /// <p>The date and time that schema was modified.</p>
